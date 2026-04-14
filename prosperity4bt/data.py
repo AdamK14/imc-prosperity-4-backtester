7289376ef6a52d15d0fa09121c5689f6e4f30149
AdamK14 <adamkhalil330@gmail.com>
2026-04-14 14:19:31 -0400

    round 1 data

--- prosperity4bt/data.py
+++ prosperity4bt/data.py
@@ -6,7 +6,9 @@ from prosperity4bt.file_reader import FileReader
 
 LIMITS = {
     "EMERALDS": 80,
-    "TOMATOES": 80
+    "TOMATOES": 80,
+    "ASH_COATED_OSMIUM": 80,
+    "INTARIAN_PEPPER_ROOT": 80
 }
 
 
